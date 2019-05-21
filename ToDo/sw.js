e1698ae7e687875754874cd4686ffe40021973de
Artem <stalker3343@gmail.com>
2019-05-21 22:34:51 +0300

    swconf

--- ToDo/sw.js
+++ ToDo/sw.js
@@ -19,15 +19,6 @@ self.addEventListener('message', (event) => {
   }
 });
 
-// self.addEventListener('activate', function (event) {
-//   event.waitUntil(
-//     caches
-//     .keys()
-//     .then(keys => keys.filter(key => !key.endsWith(version)))
-//     .then(keys => Promise.all(keys.map(key => caches.delete(key))))
-//   );
-// });
-
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
  * requests for URLs in the manifest.
@@ -38,7 +29,7 @@ self.__precacheManifest = [{
     "revision": "9e0e10750bdba57eefa5ddb9212dd216"
   },
   {
-    "url": "css/style.css?v=0.0.1",
+    "url": "css/style.css",
     "revision": "1a17068ee478f485c00fe0a699d096dd"
   },
   {
@@ -66,6 +57,38 @@ self.__precacheManifest = [{
     "revision": "04f52648a284b2b7ecb1b8cf663b710f"
   },
   {
+    "url": "images/icons/icon-128x128.png",
+    "revision": "4f6607f41105e4764ab22c263e8dc85a"
+  },
+  {
+    "url": "images/icons/icon-144x144.png",
+    "revision": "ef685a04faea7c419057823fe93b4154"
+  },
+  {
+    "url": "images/icons/icon-152x152.png",
+    "revision": "fef006812fdb8388d44e8f3eff17b224"
+  },
+  {
+    "url": "images/icons/icon-192x192.png",
+    "revision": "0c6522957583983e29a40271ecbc77ff"
+  },
+  {
+    "url": "images/icons/icon-384x384.png",
+    "revision": "ff5edf118ed4d9dbc2009e1a7870d710"
+  },
+  {
+    "url": "images/icons/icon-512x512.png",
+    "revision": "341fc92dacd0804a8fbe7f6147a5b81f"
+  },
+  {
+    "url": "images/icons/icon-72x72.png",
+    "revision": "4c2e816d5e5344a73363a47252714c6b"
+  },
+  {
+    "url": "images/icons/icon-96x96.png",
+    "revision": "f288fc71327bab2cd84c6b9e4a7cc689"
+  },
+  {
     "url": "images/trash-solid.png",
     "revision": "525cb07302fa945b1becde7a074be6cf"
   },
@@ -73,14 +96,25 @@ self.__precacheManifest = [{
     "url": "images/trash-solid.svg",
     "revision": "a3e4c2f7c32ce9a7ad6fa8d381b9d9cf"
   },
-
   {
-    "url": "js/ivenMob.js?v=0.0.1",
+    "url": "index.html",
+    "revision": "e9c52a84b8c3bdca4900fd51e5470261"
+  },
+  {
+    "url": "js/ivenMob.js",
     "revision": "5f3fe84f0445fc4cefca1133c0e1e68b"
   },
   {
-    "url": "js/script.js?v=0.0.1",
+    "url": "js/script.js",
     "revision": "7b4fd68b45861470dc1c4d16d622e23d"
+  },
+  {
+    "url": "manifest.json",
+    "revision": "f3769bb1348a68e6a72e0efde47a383f"
+  },
+  {
+    "url": "package.json",
+    "revision": "fabbec1a100e6f674ef946e00c549aee"
   },
   {
     "url": "Выводы.txt",
--- workbox-config.js
+++ workbox-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   "globDirectory": "ToDo/",
   "globPatterns": [
-    "**/*.{css,jpg,png,svg,html,js,txt}"
+    "**/*.{css,jpg,png,svg,html,js,json,txt}"
   ],
   "swDest": "ToDo\\sw.js"
 };
