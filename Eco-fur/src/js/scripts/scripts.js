80f473dd2a6873e5891ada6361da941fb03a3326
Artem <stalker3343@gmail.com>
2019-06-18 23:29:41 +0300

    shubiver2

---
+++ Eco-fur/src/js/scripts/scripts.js
@@ -0,0 +1,16 @@
+var popup = document.getElementById('popup'),
+  call = document.getElementById('call'),
+  close = document.getElementById('close');
+
+call.addEventListener('click', () => {
+  popup.style.display = 'block';
+});
+close.addEventListener('click', () => {
+  popup.style.display = 'none';
+});
+
+window.onclick = function (event) {
+  if (event.target == popup) {
+    popup.style.display = "none";
+  }
+};
