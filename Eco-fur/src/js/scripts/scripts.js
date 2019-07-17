b8cdad4498436a30149529cd816eea52e32082c7
Артём <stalker3343@gmail.com>
2019-07-17 18:47:49 +0300

    accordion

--- Eco-fur/src/js/scripts/scripts.js
+++ Eco-fur/src/js/scripts/scripts.js
@@ -1,20 +1,40 @@
-$(document).ready(function () {
-  $(".set > a").on("click", function () {
-    if ($(this).hasClass('active')) {
-      $(this).removeClass("active");
-      $(this).siblings('.content').slideUp(200);
-      $(".set > a i").removeClass("fa-minus").addClass("fa-plus");
-    } else {
-      $(".set > a i").removeClass("fa-minus").addClass("fa-plus");
-      $(this).find("i").removeClass("fa-plus").addClass("fa-minus");
-      $(".set > a").removeClass("active");
-      $(this).addClass("active");
-      $('.content').slideUp(200);
-      $(this).siblings('.content').slideDown(200);
-    }
-  });
-});
+// $(document).ready(function () {
+//   $(".set > a").on("click", function () {
+//     if ($(this).hasClass('active')) {
+//       $(this).removeClass("active");
+//       $(this).siblings('.content').slideUp(200);
+//       $(".set > a i").removeClass("fa-minus").addClass("fa-plus");
+//     } else {
+//       $(".set > a i").removeClass("fa-minus").addClass("fa-plus");
+//       $(this).find("i").removeClass("fa-plus").addClass("fa-minus");
+//       $(".set > a").removeClass("active");
+//       $(this).addClass("active");
+//       $('.content').slideUp(200);
+//       $(this).siblings('.content').slideDown(200);
+//     }
+//   });
+// });
 window.addEventListener("DOMContentLoaded", function () {
+
+
+  var accordion = new Accordion('.accordion-container');
+  var accordion2 = new Accordion('.accordion-container2');
+
+  // $(".set > a").on("click", function () {
+  //   if ($(this).hasClass('active')) {
+  //     $(this).removeClass("active");
+  //     $(this).siblings('.content').slideUp(200);
+  //     $(".set > a i").removeClass("fa-minus").addClass("fa-plus");
+  //   } else {
+  //     $(".set > a i").removeClass("fa-minus").addClass("fa-plus");
+  //     $(this).find("i").removeClass("fa-plus").addClass("fa-minus");
+  //     $(".set > a").removeClass("active");
+  //     $(this).addClass("active");
+  //     $('.content').slideUp(200);
+  //     $(this).siblings('.content').slideDown(200);
+  //   }
+  // });
+
   function closeMenu() {
     navWrap.classList.remove('nav-wrap__open');
     nav.classList.remove('nav__open')
@@ -95,8 +115,5 @@ window.addEventListener("DOMContentLoaded", function () {
     elements_selector: ".lazy"
     // ... more custom settings?
   });
-  // (function () {
-  //   // Initialize
-  //   var bLazy = new Blazy();
-  // })();
+
 });
