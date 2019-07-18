34c69de18512df5651dae3972160332b3da385a1
Артём <stalker3343@gmail.com>
2019-07-18 12:38:23 +0300

    addZakazModal

--- Eco-fur/src/js/scripts/scripts.js
+++ Eco-fur/src/js/scripts/scripts.js
@@ -1,9 +1,15 @@
 window.addEventListener("DOMContentLoaded", function () {
 
-
   var accordion = new Accordion('.accordion-container');
   var accordion2 = new Accordion('.accordion-container2');
 
+  let openMenuBtn = document.querySelector('.header__menu-link');
+  let navWrap = document.querySelector('.nav-wrap');
+  let closeBtn = document.querySelector('.nav__close');
+  let nav = document.querySelector('.nav');
+  openMenuBtn.addEventListener('click', openMenu)
+  navWrap.addEventListener('click', closeMenu)
+  closeBtn.addEventListener('click', closeMenu)
 
   function closeMenu() {
     navWrap.classList.remove('nav-wrap__open');
@@ -17,24 +23,44 @@ window.addEventListener("DOMContentLoaded", function () {
     nav.classList.add('nav__open')
   }
 
-  let openMenuBtn = document.querySelector('.header__menu-link');
-  let navWrap = document.querySelector('.nav-wrap');
-  let closeBtn = document.querySelector('.nav__close');
-  let nav = document.querySelector('.nav');
-  let akcia = document.querySelector('.akciaModal');
 
-  setTimeout(function () {
-    akcia.style.display = "flex";
-  }, 3000)
 
-  akcia.addEventListener('click', function (e) {
-    if (e.target != document.querySelector(".akciaModal__img"))
-      akcia.style.display = "none"
-  }, false)
 
-  openMenuBtn.addEventListener('click', openMenu)
-  navWrap.addEventListener('click', closeMenu)
-  closeBtn.addEventListener('click', closeMenu)
+
+  // let akcia = document.querySelector('.akciaModal');
+
+  // setTimeout(function () {
+  //   akcia.style.display = "flex";
+  // }, 3000)
+
+  // akcia.addEventListener('click', function (e) {
+  //   if (e.target != document.querySelector(".akciaModal__img"))
+  //     akcia.style.display = "none"
+  // }, false)
+
+  let zakazModal = document.querySelector('.zakazModal');
+
+  let openZakazBtn = document.querySelector('.banner__button')
+  let zakazModalWraper = document.querySelector('.zakazModal__wraper')
+  let zakazModalClose = document.querySelector('.zakazModal__close')
+
+  openZakazBtn.addEventListener('click', function () {
+    zakazModal.style.display = "flex";
+    zakazModalWraper.classList.add('zakazModal__show')
+  })
+
+  function closeZakazModal() {
+    zakazModal.style.display = "none"
+    zakazModalWraper.classList.remove('zakazModal__show')
+  }
+  zakazModalClose.addEventListener('click', closeZakazModal)
+  zakazModal.addEventListener('click', closeZakazModal)
+  zakazModalWraper.addEventListener('click', function () {
+    event.stopPropagation()
+  })
+
+
+
 
 
 
@@ -76,50 +102,34 @@ window.addEventListener("DOMContentLoaded", function () {
       if (this.value.length == 2) this.value = ""
     } else setCursorPosition(this.value.length, this)
   };
-  var input = document.querySelector(".contacts__input");
-  input.addEventListener("input", mask, false);
-  input.addEventListener("focus", mask, false);
-  input.addEventListener("blur", mask, false);
+
+  let input = document.querySelectorAll(".tell");
+
+  input.forEach(function (el) {
+    el.addEventListener("input", mask, false);
+    el.addEventListener("focus", mask, false);
+    el.addEventListener("blur", mask, false);
+  })
+
 
   const lazyLoadInstance = new LazyLoad({
     elements_selector: ".lazy"
-    // ... more custom settings?
+
   });
 
-  // const options = {
-  //   threshold: 0.1
-  // }
-  // let AlreadyInsert = false;
   let mapCont = document.querySelector('.map-cont')
 
 
-  // const callback = function (entries, observer) {
 
-  //   entries.forEach(el => {
-  //     if (el.isIntersecting) {
-  //       if (!AlreadyInsert) {
   setTimeout(function () {
     let elem = document.createElement('script')
-    elem.setAttribute("src", "https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3Addadc5d30f7af11fee01efcdb3af229c494f5ae2cf768a7e1f4131b9d8bb1366&amp;width=100%25&amp;height=500&amp;lang=ru_UA&amp;scroll=true")
+    elem.setAttribute("src", "https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3Addadc5d30f7af11fee01efcdb3af229c494f5ae2cf768a7e1f4131b9d8bb1366&amp;width=100%25&amp;height=500&amp;lang=ru_UA&amp;scroll=false")
     elem.setAttribute("type", "text/javascript")
     elem.setAttribute("charset", "utf-8")
-    elem.setAttribute("charset", "utf-8")
+    elem.setAttribute("defer", "defer")
     mapCont.appendChild(elem)
   }, 4000)
 
-
-  //         AlreadyInsert = true;
-  //       }
-
-  //     }
-  //   });
-
-
-  // };
-  // const observer = new IntersectionObserver(callback, options);
-
-  // const target = document.querySelector('.order');
-  // observer.observe(target);
 
 
 });
