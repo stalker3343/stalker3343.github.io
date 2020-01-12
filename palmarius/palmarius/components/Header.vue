22a0c4be762fd430ba0f35c12c2e64a071ab090d
Artem Byastik <stalker3343@gmail.com>
2020-01-12 16:39:32 +0300

    fix iphone 5se

---
+++ palmarius/palmarius/components/Header.vue
@@ -0,0 +1,25 @@
+  
+  <template>
+  <header class="header">
+    <div class="container">
+      <div class="header__logo logo">
+        <div class="logo__img">
+          <img src="/Palm_color.svg" alt />
+        </div>
+        <div class="logo__text"></div>
+      </div>
+
+      <div class="header__logo"></div>
+    </div>
+  </header>
+</template>
+ 
+<style>
+.header {
+  padding: 20px 0px;
+  background: rgb(221, 221, 221);
+}
+.logo__img {
+  width: 200px;
+}
+</style>
---
+++ palmarius/palmarius/pages/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <Header></Header>
+    <!-- <app-logo /> -->
+    <h1 class="title">palmarius</h1>
+    <h2 class="subtitle">Nuxt.js project</h2>
+    <div class="links">
+      <a href="https://nuxtjs.org/" target="_blank" class="button--green">Documentation</a>
+      <a href="https://github.com/nuxt/nuxt.js" target="_blank" class="button--grey">GitHub</a>
+    </div>
+  </div>
+</template>
+
+<script>
+// import AppLogo from '~/components/AppLogo.vue'
+
+import Header from "~/components/Header.vue";
+
+export default {
+  components: {
+    Header
+  }
+};
+</script>
+
+<style>
+</style>
+
